768926b3d2813dfdcc61248cfb8e3d8820e7f458
Sherlock Lau <liuz1226@hotmail.com>
2024-11-19 11:03:43 +0800

    Update unocss.config.ts

--- unocss.config.ts
+++ unocss.config.ts
@@ -104,6 +104,7 @@ export default defineConfig({
         sans: 'Inter:400,600,800',
         mono: 'DM Mono:400,600',
         condensed: 'Roboto Condensed',
+        customFontName: 'Montserrat Alternates',
       },
     }),
   ],
